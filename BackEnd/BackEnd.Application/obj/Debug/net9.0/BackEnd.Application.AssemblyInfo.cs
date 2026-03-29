c34595d47bcd3f0a9e1619b1e0c655a451c4f6aa
Nickolas Passos Homem de Mello <nickolasphmello10@gmail.com>
2026-03-28 22:03:50 -0300

    (Front) Users an Login implementention
    Implementamos no nosso front-end a tela de login e a tela de usuarios com modal para mudança.

--- BackEnd/BackEnd.Application/obj/Debug/net9.0/BackEnd.Application.AssemblyInfo.cs
+++ BackEnd/BackEnd.Application/obj/Debug/net9.0/BackEnd.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackEnd.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4763c9ae551e235acf82920f6c2af9a9dfe2223")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e98a1bd9786fcfb45cdd9c2cb86acbd92c906cba")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackEnd.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackEnd.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
